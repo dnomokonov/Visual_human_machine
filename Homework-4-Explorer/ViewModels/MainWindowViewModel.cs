49bb84b523869fb19895e60b7803abdefb189f7e
dnomokonov <zmnomokon715@gmail.com>
2024-02-25 23:38:50 +0700

    fixed

--- Homework-4-Explorer/ViewModels/MainWindowViewModel.cs
+++ Homework-4-Explorer/ViewModels/MainWindowViewModel.cs
@@ -8,6 +8,9 @@ using System.Reactive;
 using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using Avalonia.Interactivity;
+using Avalonia.Input;
+using Avalonia.Platform;
+using System.ComponentModel;
 
 namespace Homework_4_Explorer.ViewModels;
 
@@ -48,6 +51,7 @@ public class ExplorerFileMenager : ObservableObject
     private ObservableCollection<DirCollection>? _directories;
     private string _currentpath = Directory.GetCurrentDirectory();
     private DirCollection? _selectedDir;
+    private int _statusShow = 0;
 
     public ObservableCollection<DirCollection>? Directories
     {
@@ -64,33 +68,32 @@ public class ExplorerFileMenager : ObservableObject
     private void showDirectory()
     {
         Directories = new ObservableCollection<DirCollection>();
-        string? previousPath = Directory.GetParent(_currentpath)?.FullName;
-
-        if (previousPath != null)
+        Directories.Clear();
+        if (_statusShow == 0)
         {
-            Directories.Add(new DirCollection { Name = "..", PathFile = previousPath, IsDirectory = true, ImagePath = new Bitmap("~../../../../../Assets/folder.png") });
+            string? previousPath = Directory.GetParent(_currentpath)?.FullName;
+
+            Directories.Add(new DirCollection { Name = "..", PathFile = previousPath, IsDirectory = true, ImagePath = new Bitmap(AssetLoader.Open(new Uri("avares://Homework-4-Explorer/Assets/forderUp.png"))) });
+            
+            foreach (var d in Directory.GetDirectories(_currentpath))
+            {
+                Directories.Add(new DirCollection { Name = Path.GetFileName(d), PathFile = d, IsDirectory = true, ImagePath = new Bitmap(AssetLoader.Open(new Uri("avares://Homework-4-Explorer/Assets/folder.png"))) });
+            }
+
+            foreach (var f in Directory.GetFiles(_currentpath))
+            {
+                Directories.Add(new DirCollection { Name = Path.GetFileName(f), PathFile = f, IsDirectory = false, ImagePath = new Bitmap(AssetLoader.Open(new Uri("avares://Homework-4-Explorer/Assets/file.png"))) });
+            }
+        } else if (_statusShow == 1)
+        {
+            foreach (var disk in DriveInfo.GetDrives())
+            {
+                Directories.Add(new DirCollection { Name = disk.Name, PathFile = disk.RootDirectory.ToString(), IsDirectory = true, ImagePath = new Bitmap(AssetLoader.Open(new Uri("avares://Homework-4-Explorer/Assets/hardisk.png"))) });
+            }
+
+            _statusShow = 0;
         }
 
-        foreach (var d in Directory.GetDirectories(_currentpath))
-        {
-            Directories.Add(new DirCollection { Name = Path.GetFileName(d), PathFile = d, IsDirectory = true, ImagePath = new Bitmap("~../../../../../Assets/folder.png") });
-        }
-
-        foreach (var f in Directory.GetFiles(_currentpath))
-        {
-            Directories.Add(new DirCollection { Name = Path.GetFileName(f), PathFile = f, IsDirectory = false, ImagePath = new Bitmap("~../../../../../Assets/file.png") });
-        }
-
-    }
-
-    private void ShowDrives()
-    {
-        Directories = new ObservableCollection<DirCollection>();
-
-        foreach (var disk in DriveInfo.GetDrives())
-        {
-            Directories.Add(new DirCollection { Name = disk.Name, PathFile = disk.RootDirectory.ToString(), IsDirectory = true, ImagePath = new Bitmap("~../../../../../Assets/hardisk.png") });
-        }
     }
 
     public DirCollection? SelectedDir
@@ -108,14 +111,13 @@ public class ExplorerFileMenager : ObservableObject
 
     private void Add(DirCollection newpath)
     {
-        if (newpath == null || newpath.PathFile == null)
-        {
-            return;
-        }
-
         if (newpath.IsDirectory == true && !string.IsNullOrEmpty(newpath.PathFile))
         {
             _currentpath = newpath.PathFile;
+        } 
+        else if (newpath.Name == ".." && Directory.GetParent(_currentpath) == null)
+        {
+            _statusShow = 1;
         }
 
         showDirectory();
--- Homework-4-Explorer/Views/MainWindow.axaml.cs
+++ Homework-4-Explorer/Views/MainWindow.axaml.cs
@@ -1,12 +1,30 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Homework_4_Explorer.ViewModels;
 
 namespace Homework_4_Explorer.Views;
 
 public partial class MainWindow : Window
 {
+
+    private readonly ExplorerFileMenager Explorer;
+
     public MainWindow()
     {
         InitializeComponent();
+        Explorer = new ExplorerFileMenager();
+        DataContext = Explorer;
     }
+
+    private void OnListBoxDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (e.Source is Control { DataContext: DirCollection datacontext })
+        {
+            if (sender is ListBox { DataContext: MainWindowViewModel viewModel })
+            {
+                viewModel.Explorer.SelectedDir = datacontext;
+            }
+        }
+    }
+
 }
