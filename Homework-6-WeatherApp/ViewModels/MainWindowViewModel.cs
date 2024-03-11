74e885a5e036038b21ee6a614627e5edcc5a2d77
dnomokonov <zmnomokon715@gmail.com>
2024-03-11 10:11:31 +0700

    add homework6

---
+++ Homework-6-WeatherApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Avalonia.Threading;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Homework_6_WeatherApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string? _city = "Moscow";
+    private List<ShowContentViewModel.WeatherVariable>? weather = new List<ShowContentViewModel.WeatherVariable>();
+    public DateViewModel dateViewModel { get; }
+    public ClientRestAPIViewModel ClientRestAPI { get; }
+
+    public MainWindowViewModel()
+    {
+        dateViewModel = new DateViewModel();
+        ClientRestAPI = new ClientRestAPIViewModel();
+
+        Task.Run(async () =>
+        {
+            await UpdateWeather();
+        });
+    }
+
+    public string? City
+    {
+        get => _city;
+        set => _city = value;
+    }
+
+    public List<ShowContentViewModel.WeatherVariable>? Weathers
+    {
+        get { return GetTopFiveWeatherVariables(); }
+        set { this.RaiseAndSetIfChanged(ref weather, value); }
+    }
+
+    private List<ShowContentViewModel.WeatherVariable>? GetTopFiveWeatherVariables()
+    {
+        if (weather == null || weather.Count == 0)
+        {
+            return null;
+        }
+
+        return weather.Take(Math.Min(5, weather.Count)).ToList();
+    }
+
+    public async Task UpdateWeather()
+    {
+        string? json = await ClientRestAPI.GetWeatherForecastAsync(City);
+        if (json != null)
+        {
+            Weathers = ShowContentViewModel.ParseWeatherData(json);
+        }
+        else
+        {
+            Weathers = ShowContentViewModel.ParseWeatherData("");
+        }
+    }
+}
---
+++ Homework-6-WeatherApp/ViewModels/ShowContentViewModel.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using ReactiveUI;
+
+namespace Homework_6_WeatherApp.ViewModels
+{
+	public class ShowContentViewModel : ReactiveObject
+	{
+        public class WeatherVariable
+        {
+            public DateTime? Date { get; set; }
+            public DateTime? Time { get; set; }
+            public string? TempMin { get; set; }
+            public string? TempMax { get; set; }
+            public string? TempFeel { get; set; }
+            public string? Pressure { get; set; }
+            public string? Humidity { get; set; }
+            public string? WindSpeed { get; set; }
+            public double? Pop { get; set; }
+            //public Bitmap? Icon { get; set; }
+        }
+
+        public static List<WeatherVariable> ParseWeatherData(string json)
+        {
+            List<WeatherVariable> weatherForecast = new List<WeatherVariable>();
+            try
+            {
+                JsonDocument doc = JsonDocument.Parse(json);
+                JsonElement root = doc.RootElement;
+                JsonElement list = root.GetProperty("list");
+
+                foreach (var elem in list.EnumerateArray())
+                {
+
+                    WeatherVariable weatherVariable = new WeatherVariable
+                    {
+                        Date = DateTimeOffset.FromUnixTimeSeconds(elem.GetProperty("dt").GetInt64()).DateTime,
+                        Time = DateTimeOffset.FromUnixTimeSeconds(elem.GetProperty("dt").GetInt64()).DateTime,
+                        TempMin = (Math.Floor(elem.GetProperty("main").GetProperty("temp_min").GetDouble())).ToString() + "°",
+                        TempMax = (Math.Ceiling(elem.GetProperty("main").GetProperty("temp_max").GetDouble())).ToString() + "°",
+                        TempFeel = (Math.Ceiling(elem.GetProperty("main").GetProperty("feels_like").GetDouble())).ToString() + "°",
+                        Pressure = (elem.GetProperty("main").GetProperty("pressure").GetInt32()).ToString() + " hPa",
+                        Humidity = (elem.GetProperty("main").GetProperty("humidity").GetInt32()).ToString() + "%",
+                        WindSpeed = (elem.GetProperty("wind").GetProperty("speed").GetDouble()).ToString() + " ms",
+                        Pop = elem.GetProperty("pop").GetDouble(),
+                        //Icon = new Bitmap(AssetLoader.Open(new Uri($"avares://Homework_6_ExternalServices/Assets/{elem.GetProperty("weather").GetProperty("icon").GetString()}.png")))
+                    };
+
+                    weatherForecast.Add(weatherVariable);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return weatherForecast;
+        }
+    }
+}
